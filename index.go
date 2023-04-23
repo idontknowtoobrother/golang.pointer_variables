154fa546ff0f7135410ea0589d9f903955ff762d
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-04-23 19:47:52 +0700

    pointer variables

--- index.go
+++ index.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	msg := "first message"
+	var msgPointer *string = &msg
+
+	fmt.Println("msg:", msg)
+	fmt.Println("pointer of msg:", *msgPointer)
+
+	changeMessagePointer(&msg, "new message")
+	fmt.Println("changed msg:", msg)
+}
+
+func changeMessagePointer(aPointer *string, newMessage string) {
+	*aPointer = newMessage
 }
